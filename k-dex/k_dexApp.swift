976e53fcdc765920f118087c7ef34c0305720a83
irfancen <irfancen.001@gmail.com>
2026-07-24 16:16:05 +0700

    Initial Commit

---
+++ k-dex/k_dexApp.swift
@@ -0,0 +1,17 @@
+//
+//  k_dexApp.swift
+//  k-dex
+//
+//  Created by irfancen on 24/07/26.
+//
+
+import SwiftUI
+
+@main
+struct k_dexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
